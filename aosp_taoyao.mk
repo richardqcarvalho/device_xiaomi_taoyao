07e198406cff5aba60915c04bac48db2bccd7609
Richard Carvalho <richardcarvalhooo@gmail.com>
2023-04-15 13:43:23 -0300

    taoyao: more fixes to aosp_taoyao.mk

--- aosp_taoyao.mk
+++ aosp_taoyao.mk
@@ -2,14 +2,15 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-TARGET_USES_AOSP_RECOVERY := true
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_FACE_UNLOCK_SUPPORTED := true
-WITH_GMS := true
-
 # Inherit some common PixelExperience stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# Inherit from taoyao device
+# $(call inherit-product, device/xiaomi/taoyao/device.mk)
+
+TARGET_USES_AOSP_RECOVERY := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
 
 PRODUCT_NAME := aosp_taoyao
 PRODUCT_DEVICE := taoyao
@@ -17,11 +18,8 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Xiaomi 12 Lite
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-TARGET_VENDOR_PRODUCT_NAME := taoyao
-TARGET_VENDOR_DEVICE_NAME := taoyao
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    TARGET_DEVICE=taoyao_global \
     PRIVATE_BUILD_DESC="taoyao_global-user 13 TKQ1.220807.001 V14.0.4.0.TLIMIXM release-keys"
 
 BUILD_FINGERPRINT := Xiaomi/taoyao_global/taoyao:13/RKQ1.211001.001/V14.0.4.0.TLIMIXM:user/release-keys
