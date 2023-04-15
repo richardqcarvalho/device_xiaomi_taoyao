fc722a1a4dc7e39f52f92df608909e8d35f535cf
Richard Carvalho <richardcarvalhooo@gmail.com>
2023-04-15 12:07:29 -0300

    taoyao: creating base files

---
+++ AndroidProducts.mk
@@ -0,0 +1,7 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/aosp_taoyao.mk
+
+COMMON_LUNCH_CHOICES := \
+    aosp_taoyao-user \
+    aosp_taoyao-userdebug \
+    aosp_taoyao-eng
---
+++ aosp_taoyao.mk
@@ -0,0 +1,25 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+TARGET_USES_AOSP_RECOVERY := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_QUICK_TAP := true
+WITH_GMS := true
+
+# Inherit some common PixelExperience stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+PRODUCT_NAME := aosp_taoyao
+PRODUCT_DEVICE := taoyao
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Xiaomi 12 Lite
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_PRODUCT=taoyao \
+    PRIVATE_BUILD_DESC="taoyao_global-user 13 TKQ1.220807.001 V14.0.4.0.TLIMIXM release-keys"
+
+BUILD_FINGERPRINT := Xiaomi/taoyao_global/taoyao:13/RKQ1.211001.001/V14.0.4.0.TLIMIXM:user/release-keys
