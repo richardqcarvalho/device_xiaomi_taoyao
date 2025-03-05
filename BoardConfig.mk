b378ecbf11158d6f75aa0a52f1655dddeb7dea99
Richard Carvalho <richardcarvalhooo@gmail.com>
2025-03-05 19:20:55 +0000

    taoyao: Removing some uncertain configs

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -11,9 +11,6 @@ TARGET_BOOTLOADER_BOARD_NAME := taoyao
 # Inherit from sm8350-common
 include device/xiaomi/sm8350-common/BoardConfigCommon.mk
 
-TARGET_USES_GRALLOC4 := true
-TARGET_USES_HWC2 := true
-
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(DEVICE_PATH)/hidl/vendor_framework_compatibility_matrix.xml \
@@ -52,7 +49,7 @@ BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.l
 TARGET_USES_NQ_NFC := true
 
 # Partitions
-BOARD_DTBOIMG_PARTITION_SIZE := 16777216
+BOARD_DTBOIMG_PARTITION_SIZE := 25165824
 
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
@@ -61,11 +58,6 @@ TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
 
 # Recovery
 TARGET_RECOVERY_UI_MARGIN_HEIGHT := 120
-
-WIFI_DRIVER_DEFAULT := qca_cld3
-CONFIG_ACS := true
-CONFIG_IEEE80211AX := true
-WIFI_FEATURE_HOSTAPD_11AX := true
 
 # Include proprietary files
 include vendor/xiaomi/taoyao/BoardConfigVendor.mk
