2f8688b619c707d2642394d1581396f914b5d803
Richard Carvalho <richardcarvalhooo@gmail.com>
2025-03-08 19:56:52 +0000

    taoyao: Small format correction

--- device.mk
+++ device.mk
@@ -12,7 +12,7 @@ PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := false
 TARGET_HAS_UDFPS := true
 
 PRODUCT_PACKAGES += \
-     libudfpshandler
+    libudfpshandler
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/display/display_id_4630946545580055170.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946545580055170.xml \
