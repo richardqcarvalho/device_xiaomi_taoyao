65783977c823b904447fa115b415e3acd16ee491
Richard Carvalho <richardcarvalhooo@gmail.com>
2025-03-11 00:52:39 +0000

    Revert "taoyao: Using original kheaders"
    This reverts commit ad2714c5e7adc5e82cc673d3d3a4c7b7fd938e9e.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -37,8 +37,7 @@ DEVICE_MANIFEST_FILE += \
     $(DEVICE_PATH)/hidl/manifest_taoyao.xml
 
 # Kernel
-TARGET_KERNEL_SOURCE := kernel/xiaomi/taoyao
-TARGET_KERNEL_CONFIG := vendor/taoyao-qgki_defconfig
+TARGET_KERNEL_CONFIG += vendor/taoyao_QGKI.config
 TARGET_NO_KERNEL_OVERRIDE := true
 BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilts/dtbo.img
 
