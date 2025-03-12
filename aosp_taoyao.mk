1494fe7504f39267dd0e24b57dfe7cd1712b6def
Richard Carvalho <richardcarvalhooo@gmail.com>
2025-03-12 12:33:03 +0000

    taoyao: Minimal fix about PixelOS config not CrDroid

--- aosp_taoyao.mk
+++ aosp_taoyao.mk
@@ -11,7 +11,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from taoyao device
 $(call inherit-product, device/xiaomi/taoyao/device.mk)
 
-# Inherit from common CrDroid configuration
+# Inherit from common PixelOS configuration
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1080
