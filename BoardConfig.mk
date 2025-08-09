ae69025f7a6c379b968b1681c044bc6a3545075f
Ammar Bahtiar <ammarbahtiarasli@gmail.com>
2025-08-09 10:05:20 +0700

    taoyao: Updating incorrect value

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -180,7 +180,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2025-06-05
+VENDOR_SECURITY_PATCH := 2025-08-05
 
 # Sepolicy
 include device/lineage/sepolicy/libperfmgr/sepolicy.mk
--- lineage_taoyao.mk
+++ lineage_taoyao.mk
@@ -27,9 +27,9 @@ SystemName := taoyao_global
 SystemDevice := taoyao
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="taoyao_global-user 14 UKQ1.231003.002 V816.0.18.0.ULIMIXM release-keys" \
+    BuildDesc="taoyao_global-user 14 UKQ1.231003.002 V816.0.21.0.ULIMIXM release-keys" \
     DeviceName=$(SystemDevice) \
     DeviceProduct=$(SystemName)
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := Xiaomi/taoyao_global/taoyao:14/UKQ1.231003.002/V816.0.18.0.ULIMIXM:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/taoyao_global/taoyao:13/UKQ1.231003.002/V816.0.21.0.ULIMIXM:user/release-keys
